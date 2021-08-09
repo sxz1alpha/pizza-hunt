57655f070b049d5fe0cc8d1d19b500ca220b7ff6
sxz1alpha <adrian.j.ulibarri@gmail.com>
2021-08-09 12:55:31 -0600

    ask bcs

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -9,21 +9,42 @@ const $newCommentForm = document.querySelector('#new-comment-form');
 
 let pizzaId;
 
+function getPizza() {
+  // get id of pizza 
+  const searchParams = new URLSearchParams(document.location.search.substring(1));
+  const pizzaId= search.searchParams.getAttribute('id');
+
+  // get pizzaIngo
+  fetch(`/api/pizzas/${pizzaId}`)
+    .then(response => {
+      if(!response.ok) {
+        throw new Error({ message: 'Something went wrong! '});
+      }
+      return response.json();
+    })
+    .then(printPizza)
+    .catch(err => {
+      console.log(err);
+      alert("Cannot find a pizza with this id! Taking you back to where it all began.");
+      window.history.back();
+    });
+};
+
 function printPizza(pizzaData) {
   console.log(pizzaData);
-
+  
   pizzaId = pizzaData._id;
-
+  
   const { pizzaName, createdBy, createdAt, size, toppings, comments } = pizzaData;
-
+  
   $pizzaName.textContent = pizzaName;
   $createdBy.textContent = createdBy;
   $createdAt.textContent = createdAt;
   $size.textContent = size;
   $toppingsList.innerHTML = toppings
-    .map(topping => `<span class="col-auto m-2 text-center btn">${topping}</span>`)
-    .join('');
-
+  .map(topping => `<span class="col-auto m-2 text-center btn">${topping}</span>`)
+  .join('');
+  
   if (comments && comments.length) {
     comments.forEach(printComment);
   } else {
@@ -35,34 +56,34 @@ function printComment(comment) {
   // make div to hold comment and subcomments
   const commentDiv = document.createElement('div');
   commentDiv.classList.add('my-2', 'card', 'p-2', 'w-100', 'text-dark', 'rounded');
-
+  
   const commentContent = `
-      <h5 class="text-dark">${comment.writtenBy} commented on ${comment.createdAt}:</h5>
-      <p>${comment.commentBody}</p>
-      <div class="bg-dark ml-3 p-2 rounded" >
-        ${
-          comment.replies && comment.replies.length
-            ? `<h5>${comment.replies.length} ${
-                comment.replies.length === 1 ? 'Reply' : 'Replies'
-              }</h5>
-        ${comment.replies.map(printReply).join('')}`
-            : '<h5 class="p-1">No replies yet!</h5>'
-        }
-      </div>
-      <form class="reply-form mt-3" data-commentid='${comment._id}'>
-        <div class="mb-3">
-          <label for="reply-name">Leave Your Name</label>
-          <input class="form-input" name="reply-name" required />
-        </div>
-        <div class="mb-3">
-          <label for="reply">Leave a Reply</label>
-          <textarea class="form-textarea form-input"  name="reply" required></textarea>
-        </div>
-
-        <button class="mt-2 btn display-block w-100">Add Reply</button>
-      </form>
+  <h5 class="text-dark">${comment.writtenBy} commented on ${comment.createdAt}:</h5>
+  <p>${comment.commentBody}</p>
+  <div class="bg-dark ml-3 p-2 rounded" >
+  ${
+    comment.replies && comment.replies.length
+    ? `<h5>${comment.replies.length} ${
+      comment.replies.length === 1 ? 'Reply' : 'Replies'
+    }</h5>
+    ${comment.replies.map(printReply).join('')}`
+    : '<h5 class="p-1">No replies yet!</h5>'
+  }
+  </div>
+  <form class="reply-form mt-3" data-commentid='${comment._id}'>
+  <div class="mb-3">
+  <label for="reply-name">Leave Your Name</label>
+  <input class="form-input" name="reply-name" required />
+  </div>
+  <div class="mb-3">
+  <label for="reply">Leave a Reply</label>
+  <textarea class="form-textarea form-input"  name="reply" required></textarea>
+  </div>
+  
+  <button class="mt-2 btn display-block w-100">Add Reply</button>
+  </form>
   `;
-
+  
   commentDiv.innerHTML = commentContent;
   $commentSection.prepend(commentDiv);
 }
@@ -70,15 +91,16 @@ function printComment(comment) {
 function printReply(reply) {
   return `
   <div class="card p-2 rounded bg-secondary">
-    <p>${reply.writtenBy} replied on ${reply.createdAt}:</p>
-    <p>${reply.replyBody}</p>
+  <p>${reply.writtenBy} replied on ${reply.createdAt}:</p>
+  <p>${reply.replyBody}</p>
   </div>
-`;
+  `;
 }
+
 
 function handleNewCommentSubmit(event) {
   event.preventDefault();
-
+  
   const commentBody = $newCommentForm.querySelector('#comment').value;
   const writtenBy = $newCommentForm.querySelector('#written-by').value;
 
@@ -114,3 +136,5 @@ $backBtn.addEventListener('click', function() {
 
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
+
+getPizza();
