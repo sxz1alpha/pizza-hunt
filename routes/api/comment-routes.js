3fdba45bcd991b9666aac8b51918720a51dda0ce
sxz1alpha <adrian.j.ulibarri@gmail.com>
2021-08-10 11:19:28 -0600

    moving to dt

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { addComment, 
+        removeComment,
+        addReply,
+        removeReply
+    } = require('../../controllers/comment-controller');
 
 router
     .route('/:pizzaId').post(addComment);
 
 router
-    .route('/:pizzaId/:commentId').delete(removeComment);
+    .route('/:pizzaId/:commentId').delete(removeComment)
+    .put(addReply)
+    .delete(removeComment)
 
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply);
 module.exports = router;
